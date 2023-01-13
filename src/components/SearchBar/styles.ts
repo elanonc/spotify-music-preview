daad1fdbbc9e3b2e6703e8d89dda73b65a3a2395
Elano N. Caitano <nunes.elano14@gmail.com>
2023-01-12 21:39:21 -0300

    Ajuste na estilização do componente SearchBar

--- src/components/SearchBar/styles.ts
+++ src/components/SearchBar/styles.ts
@@ -3,10 +3,12 @@ import styled from 'styled-components';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  padding: 0 140px;
+  margin-bottom: 75px;
 `;
 
 export const Label = styled.label`
-  font-size: var(--font-large);
+  font-size: var(--font-medium);
   color: var(--tertiary);
 `;
 
@@ -17,8 +19,10 @@ export const Input = styled.input`
   height: 80px;
   font-weight: bold;
   padding-bottom: 14px;
+  border: 0;
   border-bottom: 2px solid var(--secondary);
   background-color: transparent;
   color: #fff;
   outline: 0;
+  padding-left: 10px;
 `;
