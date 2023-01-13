c262d14a1aa22aed695221a2e601e30bb64149ee
Elano N. Caitano <nunes.elano14@gmail.com>
2023-01-12 22:53:32 -0300

    Passando as props para SearchBar

--- src/components/SearchBar/SearchBar.tsx
+++ src/components/SearchBar/SearchBar.tsx
@@ -1,10 +1,17 @@
 import { Container, Label, Input } from './styles';
 
-export function SearchBar() {
+type SearchBarProps = {
+  handleSetSearchTerm: (prop: string) => void;
+  searchTerm: string | undefined;
+}
+
+export function SearchBar({ searchTerm, handleSetSearchTerm }: SearchBarProps) {
   return (
     <Container>
       <Label>Busque por artistas, álbuns ou músicas.</Label>
       <Input
+        value={searchTerm}
+        onChange={(event)=>handleSetSearchTerm(event.target.value)}
         type="text"
         placeholder="Comece a escrever..."
       />
